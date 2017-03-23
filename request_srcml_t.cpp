d4a486d302601430d7839de999ac4cbb56db1167
ryankody <rsk35@zips.uakron.edu>
2017-03-23 13:06:41 -0400

    Added second test case for filename

--- request_srcml_t.cpp
+++ request_srcml_t.cpp
@@ -16,10 +16,9 @@ int main() {
     	assert(request_filename(request) == "");
     	assert(request_language(request, "") == "");
 	
-	//make sure empty request works
-	srcml_request request2 = { "", "", "", ""};
-    	assert(request_filename(request) == "");
-    	assert(request_language(request, "") == "");
+	//make sure the file name is correct from a request
+	srcml_request request2 = { "file.cpp", "", "", ""};
+    	assert(request_filename(request) == "file");
     }
 
     return 0;
